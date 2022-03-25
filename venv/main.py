368d50d733aa6ed520f32104270ec919829ff932
mina-beiki <mina_beiki@yahoo.com>
2022-03-25 14:52:39 +0430

    working on part2

--- venv/main.py
+++ venv/main.py
@@ -27,34 +27,35 @@ def convolution(x, h):
 # plt.show()
 
 #signal b:
-# start1 = 0
-# end1 = 4
-# start2 = 5
-# end2 = 10
-# x1_2 = np.linspace(start1, end1, end1-start1+1)
-# y1_2 = -x1-1
-# x2_2 = np.linspace(start2, end2, end2-start2+1)
-# y2_2 = np.power(x2_2, 2)
-# style.use('ggplot')
-# plt.stem(x1_2, y1)
-# plt.stem(x2_2, y2)
-# plt.title('2')
-# plt.xlabel('X-Axis')
-# plt.ylabel('Y-Axis')
-# plt.legend()
-# plt.show()
-
-#part3:
-
-n3 = np.arange(-10, 10, 1)
-x3 = np.power(1/3, n3)*(np.heaviside(n3, 1)-np.heaviside(n3-10, 1))
-h3 = np.power(0.9, n3)*(np.heaviside(n3-5, 1)-np.heaviside(n3, 1))
-y3 = convolution(x3, h3)
-y3 = y3[int(len(y3)/4) : -1 * int(len(y3)/4) -1]
+start1 = 0
+end1 = 4
+start2 = 5
+end2 = 10
+step = 0.01
+x1_2 = np.linspace(start1, end1, int((end1 - start1)/step))
+y1_2 = x1_2
+x2_2 = np.linspace(start2, end2, int((end2 - start2)/step))
+y2_2 = 5 - x2_2
 style.use('ggplot')
-plt.stem(n3, y3)
-plt.title('Part 3')
+plt.plot(x1_2, y1_2)
+plt.plot(x2_2, y2_2)
+plt.title('b')
 plt.xlabel('X-Axis')
 plt.ylabel('Y-Axis')
 plt.legend()
 plt.show()
+
+#part3:
+
+# n3 = np.arange(-10, 10, 1)
+# x3 = np.power(1/3, n3)*(np.heaviside(n3, 1)-np.heaviside(n3-10, 1))
+# h3 = np.power(0.9, n3)*(np.heaviside(n3-5, 1)-np.heaviside(n3, 1))
+# y3 = convolution(x3, h3)
+# y3 = y3[int(len(y3)/4) : -1 * int(len(y3)/4) -1]
+# style.use('ggplot')
+# plt.stem(n3, y3)
+# plt.title('Part 3')
+# plt.xlabel('X-Axis')
+# plt.ylabel('Y-Axis')
+# plt.legend()
+# plt.show()
